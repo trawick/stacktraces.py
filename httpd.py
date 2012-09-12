fc78b7db19234f813e25e27e868c8eb743c1ce7e
Jeff Trawick <trawick@gmail.com>
2012-09-12 08:32:50 -0400

    allow more specific annotations and called-indirectly-by conditions

--- httpd.py
+++ httpd.py
@@ -8,8 +8,12 @@
 annotations = [
 ('t', ['cdb', 'listener_thread', 'dummy_worker'], 'MPM child listener thread'),
 ('t', ['cdb', 'worker_thread', 'dummy_worker'], 'MPM child worker thread'),
-('t', ['cdb', 'ap_event_pod_check', 'child_main'], 'MPM child main thread'),
-('t', ['cdb', 'ap_mpm_pod_check', 'child_main'], 'MPM child main thread'),
+('t', ['cib', 'child_main', 'event_run'], 'Event MPM child main thread'),
+# less specific
+('t', ['is', 'child_main'], 'MPM child main thread'),
+('t', ['cib', 'ap_wait_or_timeout', 'event_run'], 'Event MPM parent'),
+# less specific
+('t', ['is', 'ap_wait_or_timeout'], 'MPM parent'),
 ('s', ['cdb', 'ap_queue_pop_something', 'worker_thread'], 'waiting for connection to handle'),
 ('s', ['cdb', 'ap_queue_pop', 'worker_thread'], 'waiting for connection to handle'),
 ('s', ['cdb', 'apr_pollset_poll', 'listener_thread'], 'waiting for connection to accept'),
@@ -23,6 +27,7 @@ annotations = [
 
 cleanups = [
 ('db', ['cdb', 'apr_thread_cond_wait', 'ap_queue_pop']),
+('db', ['is', 'apr_sleep']),
 ('da', ['is', 'dummy_worker']),
 ('db', ['is', 'apr_proc_mutex_lock']),
 ('db', ['is', 'ap_mpm_pod_check']),
@@ -41,6 +46,14 @@ def check_condition(cond, t):
             if cond[0] == 'cdb':
                 if i + 1 < len(t.frames) and t.frames[i + 1].fn == cond[2]:
                     return (i, i + 1)
+            elif cond[0] == 'cib':
+                j = i + 1
+                while j < len(t.frames):
+                    print 'comparing %s and %s' % (t.frames[j].fn, cond[2])
+                    if t.frames[j].fn == cond[2]:
+                        print 'found'
+                        return (i, j)
+                    j += 1
             else:
                 raise Exception('Unexpected condition type >%s<' % cond[0])
     return None
@@ -59,18 +72,23 @@ def cleanup(p):
     
 def annotate(p):
     for t in p.threads:
+        found = {} # don't test less-specific annotations of this type
         for a in annotations:
             if a[0] == 't':
-                # set thread name
-                cond = a[1]
-                tname = a[2]
-                if check_condition(cond, t) != None:
-                    t.set_name(tname)
+                if not 't' in found:
+                    # set thread name
+                    cond = a[1]
+                    tname = a[2]
+                    if check_condition(cond, t) != None:
+                        found['t'] = True
+                        t.set_name(tname)
             elif a[0] == 's':
-                cond = a[1]
-                sname = a[2]
-                if check_condition(cond, t) != None:
-                    t.set_state(sname)
+                if not 's' in found:
+                    cond = a[1]
+                    sname = a[2]
+                    if check_condition(cond, t) != None:
+                        found['s'] = True
+                        t.set_state(sname)
             else:
                 raise Exception('Unexpected annotation type >%s<' % a[0])
 
