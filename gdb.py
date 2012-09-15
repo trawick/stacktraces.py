319ce88ed1eefee2a4d16d16dec6f5d1defd504e
Jeff Trawick <trawick@gmail.com>
2012-09-15 08:57:18 -0400

    add copyright/license, check for unintended use as __main__, zap old upstack.py

--- gdb.py
+++ gdb.py
@@ -1,3 +1,18 @@
+# Copyright 2012 Jeff Trawick, http://emptyhammock.com/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import re
 import subprocess
 import sys
@@ -64,3 +79,6 @@ class gdb:
 
     def get_output(self):
         self.gdbout = collect.gdb_collect(None, self.pid, self.corefile, self.exe)
+
+if __name__ == "__main__":
+    print >> sys.stderr, "Don't run this directly."
