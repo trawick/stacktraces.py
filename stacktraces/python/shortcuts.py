1937cebe0080435f8790a925e1f83870860c6765
Jeff Trawick <trawick@gmail.com>
2016-01-12 20:12:22 -0500

    Rearrange code for testing Python log file parsing

--- stacktraces/python/shortcuts.py
+++ stacktraces/python/shortcuts.py
@@ -1,6 +1,8 @@
+import re
+
 from six import text_type
 
-from stacktraces import process_model
+from stacktraces import process_model, thread_analyzer
 from stacktraces.python import stacktrace
 
 
@@ -14,3 +16,89 @@ def describe_lines(traceback_lines):
     # thread_analyzer.annotate(p, my_annotations)
     p.group()  # only one thread, but this allows str(p) to work
     return text_type(p)
+
+
+TRACE_MSG_RE_1 = re.compile(r'^\[([^]]+)\] ERROR \[[^]]+\] (.*)\n?$')
+TRACE_MSG_RE_2 = re.compile(r'^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d).*(INFO|WARNING|ERROR) +(.*)\n?$')
+
+
+def parse_trace_msg(msg):
+    m = TRACE_MSG_RE_1.match(msg)
+    if m:
+        timestamp, error_msg = m.groups()
+    else:
+        m = TRACE_MSG_RE_2.match(msg)
+        if m:
+            timestamp, _, error_msg = m.groups()
+        else:
+            timestamp, error_msg = None, None
+    return timestamp, error_msg
+
+
+def handle_traceback(traceback_lines, msg, tracelvl, handler, cleanups, annotations):
+    # We just have a traceback from an individual thread, so skip:
+    # . ProcessGroup representation
+    # . Process.group(), which finds threads in a process with same backtrace
+
+    timestamp, error_msg = parse_trace_msg(msg)
+    # if not timestamp:
+    #     raise ValueError('Cannot parse log message "%s"' % msg)
+
+    # Ignore error message in the related log message for now; it seems to be
+    # always duplicated within the traceback output
+    p = process_model.Process(0)
+    ptb = stacktrace.PythonTraceback(
+        proc=p, lines=traceback_lines, timestamp=timestamp, name='WhatNameHere?'
+    )
+    ptb.parse()
+    thread_analyzer.cleanup(p, cleanups)
+    thread_analyzer.annotate(p, annotations)
+    p.group()  # only one thread, but this allows str(p) to work
+    if tracelvl > 1:
+        print('-------------')
+        print(traceback_lines)
+    handler(process=p)
+
+
+class Line(object):
+    def __init__(self, line):
+        self.line = line
+        self.is_traceback = line.startswith('Traceback ')
+        self.is_log_msg = False
+        if not self.is_traceback:
+            timestamp, _ = parse_trace_msg(line)
+            if timestamp:
+                self.is_log_msg = True
+
+
+class ParseState(object):
+    def __init__(self):
+        self.in_traceback = False
+        self.traceback_lines = []
+        self.traceback_log_msg = None
+
+
+def read_log(tracelvl, logfile, handler, cleanups=(), annotations=()):
+    prev = None
+    s = ParseState()
+
+    while True:
+        l = logfile.readline()
+        if l == '':
+            break
+        l = Line(l)
+        if l.is_traceback:
+            if s.in_traceback:
+                handle_traceback(s.traceback_lines, s.traceback_log_msg, tracelvl, handler, cleanups, annotations)
+                s = ParseState()
+            s.in_traceback = True
+            s.traceback_log_msg = prev.line
+        elif l.is_log_msg and s.traceback_lines:
+            handle_traceback(s.traceback_lines, s.traceback_log_msg, tracelvl, handler, cleanups, annotations)
+            s = ParseState()
+        if s.in_traceback:
+            s.traceback_lines.append(l.line)
+        prev = l
+    if s.in_traceback:
+        handle_traceback(s.traceback_lines, s.traceback_log_msg, tracelvl, handler, cleanups, annotations)
+        # s = ParseState()
--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,6 +1,8 @@
 import unittest
 
-from stacktraces.python.shortcuts import describe_lines
+import six
+
+from stacktraces.python.shortcuts import describe_lines, read_log
 
 PYTHON_STACKTRACE = u"""[14/Mar/2015 01:37:05] ERROR [django.request:231] Internal Server Error: /walk/ExYu
 Traceback (most recent call last):
@@ -35,6 +37,63 @@ error: [Errno 110] Connection timed out""".split('\n')
 EXPECTED_PYTHON_STACKTRACE = u'1 * [0] Python Exception, Failure was <error: [Errno 110] Connection timed out>\n  get_response, _wrapped_view, index, weather_forecast, around, _fetch, request, _send_request, endheaders, _send_output, send, connect, create_connection, \n'
 
 
-class TestHttpdLogs(unittest.TestCase):
+class TestPythonStacktrace(unittest.TestCase):
     def test_describe_python_stacktrace(self):
         self.assertEqual(describe_lines(PYTHON_STACKTRACE), EXPECTED_PYTHON_STACKTRACE)
+
+
+LOG_FILE_CONTENTS = u"""
+[14/Mar/2015 01:37:05] ERROR [django.request:231] Internal Server Error: /walk/ExYu
+Traceback (most recent call last):
+  File "/home/trawick/git/walking/envs/walking/lib/python2.7/site-packages/django/core/handlers/base.py", line 111, in get_response
+    response = wrapped_callback(request, *callback_args, **callback_kwargs)
+  File "/home/trawick/git/walking/envs/walking/lib/python2.7/site-packages/django/contrib/auth/decorators.py", line 21, in _wrapped_view
+    return view_func(request, *args, **kwargs)
+  File "/home/trawick/git/walking/src/walking/walks/views.py", line 58, in index
+    forecast = wo.weather_forecast()
+  File "/home/trawick/git/walking/src/walking/walks/models.py", line 81, in weather_forecast
+    city, raw_forecast = w.around(start, 4 * 60 * 60)
+  File "/home/trawick/git/walking/src/walking/walking/weather.py", line 132, in around
+    d = Weather._fetch(uri, self._validate_around_weather)
+  File "/home/trawick/git/walking/src/walking/walking/weather.py", line 42, in _fetch
+    c.request('GET', uri)
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 995, in request
+    self._send_request(method, url, body, headers)
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 1029, in _send_request
+    self.endheaders(body)
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 991, in endheaders
+    self._send_output(message_body)
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 844, in _send_output
+    self.send(msg)
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 806, in send
+    self.connect()
+  File "/home/trawick/python-2.7/lib/python2.7/httplib.py", line 787, in connect
+    self.timeout, self.source_address)
+  File "/home/trawick/python-2.7/lib/python2.7/socket.py", line 571, in create_connection
+    raise err
+error: [Errno 110] Connection timed out
+"""
+
+
+global_buffer = []
+
+
+def save_to_buffer(process):
+    global global_buffer
+    global_buffer.append(six.text_type(process))
+
+
+class TestPythonLog(unittest.TestCase):
+    def test_simple_logfile(self):
+        global global_buffer
+        if six.PY2:
+            import StringIO
+            logfile = StringIO.StringIO(LOG_FILE_CONTENTS)
+        else:
+            import io
+            logfile = io.StringIO(LOG_FILE_CONTENTS)
+        global_buffer = []
+        read_log(tracelvl=1, logfile=logfile, handler=save_to_buffer)
+        expected = u"""1 * [0] WhatNameHere?, Failure was <error: [Errno 110] Connection timed out>, at <14/Mar/2015 01:37:05>
+  get_response, _wrapped_view, index, weather_forecast, around, _fetch, request, _send_request, endheaders, _send_output, send, connect, create_connection, \n"""
+        self.assertEqual(global_buffer, [expected])
