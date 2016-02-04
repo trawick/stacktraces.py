8a4ad273a8eb46cdaea26c67045824d53211a71f
Jeff Trawick <trawick@gmail.com>
2016-02-04 09:14:23 -0500

    Lots of improvements to extracting exceptions from log files
    and more testcases

--- stacktraces/python/shortcuts.py
+++ stacktraces/python/shortcuts.py
@@ -21,19 +21,33 @@ def describe_lines(traceback_lines):
 LOGLVL_RE = r'(CRITICAL|ERROR|WARNING|INFO|DEBUG)'
 TRACE_MSG_RE_1 = re.compile(r'^\[([^]]+)\] ' + LOGLVL_RE + ' \[[^]]+\] (.*)\n?$')
 TRACE_MSG_RE_2 = re.compile(r'^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d).*' + LOGLVL_RE + ' +(.*)\n?$')
+TRACE_MSG_RE_3 = re.compile(r'^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d) .*\[' + LOGLVL_RE + '\] +(.*)\n?')
+TRACE_MSG_RE_4 = re.compile(r'^.*\[([A-Z][a-z][a-z] [A-Z][a-z][a-z] \d\d \d\d:\d\d:\d\d \d\d\d\d)\] +(.*)\n?')
+TRACE_MSG_RE_DEFAULT = re.compile(r'^\[[^]]+\] *(.*)\n?$')
+
+TRACE_MSG_RES = [
+    (TRACE_MSG_RE_1, 1, 3,),
+    (TRACE_MSG_RE_2, 1, 3,),
+    (TRACE_MSG_RE_3, 1, 3,),
+    (TRACE_MSG_RE_4, 1, 2),
+    (TRACE_MSG_RE_DEFAULT, None, 1)
+]
 
 
 def parse_trace_msg(msg):
-    m = TRACE_MSG_RE_1.match(msg)
-    if m:
-        timestamp, _, error_msg = m.groups()
-    else:
-        m = TRACE_MSG_RE_2.match(msg)
+    for regex, timestamp_index, msg_index in TRACE_MSG_RES:
+        m = regex.match(msg)
         if m:
-            timestamp, _, error_msg = m.groups()
-        else:
-            timestamp, error_msg = None, None
-    return timestamp, error_msg
+            if timestamp_index is not None:
+                timestamp = m.group(timestamp_index)
+            else:
+                timestamp = None
+            if msg_index is not None:
+                msg = m.group(msg_index)
+            else:
+                msg = None
+            return timestamp, msg
+    return None, None
 
 
 def handle_traceback(traceback_lines, msg, tracelvl, handler, cleanups, annotations):
@@ -41,7 +55,10 @@ def handle_traceback(traceback_lines, msg, tracelvl, handler, cleanups, annotati
     # . ProcessGroup representation
     # . Process.group(), which finds threads in a process with same backtrace
 
-    timestamp, error_msg = parse_trace_msg(msg)
+    if msg:
+        timestamp, error_msg = parse_trace_msg(msg.line)
+    else:
+        timestamp, error_msg = None, None
     # if not timestamp:
     #     raise ValueError('Cannot parse log message "%s"' % msg)
 
@@ -64,16 +81,16 @@ def handle_traceback(traceback_lines, msg, tracelvl, handler, cleanups, annotati
 class Line(object):
     def __init__(self, line):
         self.line = line
-        self.is_traceback = line.startswith('Traceback ')
+        self.is_start_of_traceback = line.startswith('Traceback ')
         self.is_log_msg = False
-        if not self.is_traceback:
-            timestamp, _ = parse_trace_msg(line)
-            if timestamp:
+        if not self.is_start_of_traceback:
+            timestamp, msg = parse_trace_msg(line)
+            if timestamp or msg:
                 self.is_log_msg = True
 
     def __str__(self):
         return '%s%s%s' % (
-            'TB ' if self.is_traceback else '',
+            'TB ' if self.is_start_of_traceback else '',
             'LG ' if self.is_log_msg else '',
             self.line,
         )
@@ -91,7 +108,7 @@ class ParseState(object):
             fields.append('IN-TB')
             fields.append('%s..' % self.traceback_lines[0])
             if self.traceback_log_msg:
-                fields.append(self.traceback_log_msg)
+                fields.append(self.traceback_log_msg.line)
         return ' '.join(fields)
 
 
@@ -104,16 +121,16 @@ def read_log(tracelvl, logfile, handler, cleanups=(), annotations=()):
         if l == '':
             break
         l = Line(l)
-        if l.is_traceback:
+        if l.is_start_of_traceback:
             if s.in_traceback:
                 handle_traceback(s.traceback_lines, s.traceback_log_msg, tracelvl, handler, cleanups, annotations)
                 s = ParseState()
             s.in_traceback = True
-            s.traceback_log_msg = prev_log_msg.line
+            s.traceback_log_msg = prev_log_msg
         elif l.is_log_msg and s.traceback_lines:
             handle_traceback(s.traceback_lines, s.traceback_log_msg, tracelvl, handler, cleanups, annotations)
             s = ParseState()
-        if s.in_traceback and not l.line.startswith('['):
+        if s.in_traceback and not (l.line.startswith('[') or l.line in ('', '\n')):
             s.traceback_lines.append(l.line)
         if l.is_log_msg:
             prev_log_msg = l
--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -2,7 +2,7 @@ import unittest
 
 import six
 
-from stacktraces.python.shortcuts import describe_lines, read_log
+from stacktraces.python.shortcuts import describe_lines, parse_trace_msg, read_log
 
 PYTHON_STACKTRACE = u"""[14/Mar/2015 01:37:05] ERROR [django.request:231] Internal Server Error: /walk/ExYu
 Traceback (most recent call last):
@@ -76,8 +76,79 @@ Traceback (most recent call last):
   File "/home/trawick/python-2.7/lib/python2.7/socket.py", line 571, in create_connection
     raise err
 error: [Errno 110] Connection timed out
+
 """  # noqa
 
+LOG_FILE_CONTENTS_2 = u"""
+[something-before-exception]
+Traceback (most recent call last):
+  File "/home/trawick/foo2.py", line 22, in <module>
+    a()
+  File "/home/trawick/foo2.py", line 18, in a
+    b()
+  File "/home/trawick/foo2.py", line 14, in b
+    c()
+  File "/home/trawick/foo2.py", line 10, in c
+    d()
+  File "/home/trawick/foo2.py", line 6, in d
+    raise ValueError('aaa')
+ValueError: aaa
+bbb
+ccc
+[something-after-exception]
+"""  # noqa
+
+LOG_FILE_CONTENTS_3 = u"""
+[25/Dec/2015 12:59:55] ERROR [django.request:231] Internal Server Error: /admin/walks/walk/add/
+Traceback (most recent call last):
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/core/handlers/base.py", line 111, in get_response
+    response = wrapped_callback(request, *callback_args, **callback_kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/contrib/admin/options.py", line 583, in wrapper
+    return self.admin_site.admin_view(view)(*args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/utils/decorators.py", line 105, in _wrapped_view
+    response = view_func(request, *args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/views/decorators/cache.py", line 52, in _wrapped_view_func
+    response = view_func(request, *args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/contrib/admin/sites.py", line 206, in inner
+    return view(request, *args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/contrib/admin/options.py", line 1453, in add_view
+    return self.changeform_view(request, None, form_url, extra_context)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/utils/decorators.py", line 29, in _wrapper
+    return bound_func(*args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/utils/decorators.py", line 105, in _wrapped_view
+    response = view_func(request, *args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/utils/decorators.py", line 25, in bound_func
+    return func.__get__(self, type(self))(*args2, **kwargs2)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/transaction.py", line 394, in inner
+    return func(*args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/contrib/admin/options.py", line 1404, in changeform_view
+    self.save_model(request, new_object, form, not add)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/contrib/admin/options.py", line 1045, in save_model
+    obj.save()
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/base.py", line 589, in save
+    force_update=force_update, update_fields=update_fields)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/base.py", line 617, in save_base
+    updated = self._save_table(raw, cls, force_insert, force_update, using, update_fields)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/base.py", line 698, in _save_table
+    result = self._do_insert(cls._base_manager, using, fields, update_pk, raw)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/base.py", line 731, in _do_insert
+    using=using, raw=raw)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/manager.py", line 92, in manager_method
+    return getattr(self.get_queryset(), name)(*args, **kwargs)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/query.py", line 921, in _insert
+    return query.get_compiler(using=using).execute_sql(return_id)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/models/sql/compiler.py", line 920, in execute_sql
+    cursor.execute(sql, params)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/backends/utils.py", line 65, in execute
+    return self.cursor.execute(sql, params)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/utils.py", line 94, in __exit__
+    six.reraise(dj_exc_type, dj_exc_value, traceback)
+  File "/home/trawick/git/walking/envs/walking/local/lib/python2.7/site-packages/django/db/backends/utils.py", line 65, in execute
+    return self.cursor.execute(sql, params)
+IntegrityError: duplicate key value violates unique constraint "walks_walk_walk_group_id_key"
+DETAIL:  Key (walk_group_id, walk_datetime)=(ExYu, 2016-01-10 14:30:00+00) already exists.
+
+"""  # noqa
 
 global_buffer = []
 
@@ -87,17 +158,74 @@ def save_to_buffer(process):
     global_buffer.append(six.text_type(process))
 
 
+def file_from_string(contents):
+    if six.PY2:
+        import StringIO
+        logfile = StringIO.StringIO(contents)
+    else:
+        import io
+        logfile = io.StringIO(contents)
+    return logfile
+
+
 class TestPythonLog(unittest.TestCase):
     def test_simple_logfile(self):
         global global_buffer
-        if six.PY2:
-            import StringIO
-            logfile = StringIO.StringIO(LOG_FILE_CONTENTS)
-        else:
-            import io
-            logfile = io.StringIO(LOG_FILE_CONTENTS)
+        logfile = file_from_string(LOG_FILE_CONTENTS)
         global_buffer = []
         read_log(tracelvl=1, logfile=logfile, handler=save_to_buffer)
         expected = u"""1 * [0] WhatNameHere?, Failure was <error: [Errno 110] Connection timed out>, at <14/Mar/2015 01:37:05>
   get_response, _wrapped_view, index, weather_forecast, around, _fetch, request, _send_request, endheaders, _send_output, send, connect, create_connection, \n"""  # noqa
         self.assertEqual(global_buffer, [expected])
+
+    def test_another_logfile(self):
+        global global_buffer
+        logfile = file_from_string(LOG_FILE_CONTENTS_2)
+        global_buffer = []
+        read_log(tracelvl=1, logfile=logfile, handler=save_to_buffer)
+        expected = u"""1 * [0] WhatNameHere?, Failure was <ValueError: aaa|bbb|ccc>\n  <module>, a, b, c, d, \n"""  # noqa
+        self.assertEqual(global_buffer, [expected])
+
+    def test_yet_another_logfile(self):
+        global global_buffer
+        logfile = file_from_string(LOG_FILE_CONTENTS_3)
+        global_buffer = []
+        read_log(tracelvl=1, logfile=logfile, handler=save_to_buffer)
+        expected = u"""1 * [0] WhatNameHere?, Failure was <IntegrityError: duplicate key value violates unique constraint "walks_walk_walk_group_id_key"|DETAIL:  Key (walk_group_id, walk_datetime)=(ExYu, 2016-01-10 14:30:00+00) already exists.>, at <25/Dec/2015 12:59:55>\n  get_response, wrapper, _wrapped_view, _wrapped_view_func, inner, add_view, _wrapper, _wrapped_view, bound_func, inner, changeform_view, save_model, save, save_base, _save_table, _do_insert, manager_method, _insert, execute_sql, execute, __exit__, execute, \n"""  # noqa
+        self.assertEqual(global_buffer, [expected])
+
+    def test_parse_trace_msg(self):
+        tests = [
+            [
+                '2014-03-06 16:29:21 [4354] [INFO] Worker exiting (pid: 4354)', '2014-03-06 16:29:21',
+                'Worker exiting (pid: 4354)'
+            ],
+            [
+                '[anything]any-other-thing', None, 'any-other-thing',
+            ],
+            [
+                '[14/Mar/2015 01:37:05] ERROR [django.request:231] Internal Server Error: /walk/ExYu',
+                '14/Mar/2015 01:37:05', 'Internal Server Error: /walk/ExYu',
+            ],
+            [
+                '', None, None,
+            ],
+            [
+                'anything', None, None,
+            ],
+            [
+                '[pid: 10876|app: 0|req: 7/7] 45.37.54.3 () {70 vars in 3333 bytes} [Sat Apr 18 21:34:03 2015] GET /walk/ExYu => generated 6134 bytes in 1257 msecs (HTTP/1.1 200) 4 headers in 223 bytes (1 switches on core 0)',  # noqa
+                'Sat Apr 18 21:34:03 2015',
+                'GET /walk/ExYu => generated 6134 bytes in 1257 msecs (HTTP/1.1 200) 4 headers in 223 bytes (1 switches on core 0)',  # noqa
+            ]
+        ]
+
+        for log_line, expected_timestamp, expected_msg in tests:
+            actual_timestamp, actual_msg = parse_trace_msg(log_line)
+            self.assertEqual(expected_timestamp, actual_timestamp)
+            self.assertEqual(expected_msg, actual_msg)
+
+            # Try again with newline at end of message
+            actual_timestamp, actual_msg = parse_trace_msg(log_line + '\n')
+            self.assertEqual(expected_timestamp, actual_timestamp)
+            self.assertEqual(expected_msg, actual_msg)
