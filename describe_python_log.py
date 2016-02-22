858ebc6b94992e95816b220d15e9caec0b0c82ad
Jeff Trawick <trawick@gmail.com>
2016-02-22 08:17:35 -0500

    Suppress duplicates by default, add JSON output format

--- describe_python_log.py
+++ describe_python_log.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2015 Jeff Trawick, http://emptyhammock.com/
+# Copyright 2015, 2016 Jeff Trawick, http://emptyhammock.com/
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,22 +20,59 @@
 
 from __future__ import print_function
 
+import argparse
 import io
+import json
 import sys
 
 from stacktraces.python.shortcuts import read_log
 
+args = None
+need_delim = False
+seen = dict()
+
 
 def print_process(process):
-    print(process)
+    global args, need_delim
+
+    if not args.include_duplicates:
+        thread = process.threads[0]
+        frame_functions = [frame.fn for frame in thread.frames]
+        key = ','.join(frame_functions)
+        if key in seen:
+            return
+        seen[key] = True
+
+    if args.format == 'text':
+        print(process)
+    else:
+        if need_delim:
+            print(',')
+        print(json.dumps(process.description(wrapped=True)))
+        need_delim = True
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: %s <log file name>' % sys.argv[0], file=sys.stderr)
+    global args
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_file_name',
+                        help='name of log file to parse')
+    parser.add_argument('--format', action='store', default='text',
+                        help='output format ("json" or "text")')
+    parser.add_argument('--include-duplicates', action='store_true',
+                        help='whether to include duplicate stacktraces in output')
+    args = parser.parse_args()
+
+    if args.format != 'text' and args.format != 'json':
+        print('Wrong value for --format', file=sys.stderr)
         sys.exit(1)
 
+    if args.format == 'json':
+        print('[')
     read_log(tracelvl=1, logfile=io.open(sys.argv[1], encoding='utf8'), handler=print_process)
+    if args.format == 'json':
+        print(']')
 
 if __name__ == '__main__':
     main()
